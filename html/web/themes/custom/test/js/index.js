8ffa9f5d96c26b7ab352b7900d042619d1bd7d9e
Shimon Amar <shimona@realcommerce.co.il>
2021-04-13 23:43:44 +0300

    print

---
+++ html/web/themes/custom/test/js/index.js
@@ -0,0 +1,18 @@
+(function ($) {
+  var ua = navigator.userAgent.toLowerCase();
+  var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+
+  $('.print button').click(function(e) {
+    e.preventDefault();
+    if (isAndroid) {
+      // https://developers.google.com/cloud-print/docs/gadget
+      var gadget = new cloudprint.Gadget();
+      gadget.setPrintDocument("url", $('title').html(), window.location.href, "utf-8");
+      gadget.openPrintDialog();
+    } else {
+      window.print();
+    }
+    return false;
+  });
+
+})(jQuery);
